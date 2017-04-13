9dc49572e6165d6528ce16f4bb4b408219980cf4
Akhil Agarwal <Akhil Agarwal>
2017-04-14 03:03:41 +0530

    Maps Added

--- app/src/main/java/com/akhilagarwal96/gps/MainActivity.java
+++ app/src/main/java/com/akhilagarwal96/gps/MainActivity.java
@@ -2,39 +2,126 @@ package com.akhilagarwal96.gps;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.model.LatLng;
 
-    private Context context;
+import java.io.IOException;
+import java.util.List;
 
-    boolean isGPSenabled = false;
-    boolean isNetworkenabled = false;
-    boolean canGetLocation = false;
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
-    Location location;
-    double latitude;
-    double longitude;
+    TextView latitude;
+    TextView longitude;
+    Button b;
+    LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        latitude = (TextView) findViewById(R.id.text_latValue);
+        longitude = (TextView) findViewById(R.id.text_longValue);
+        b = (Button) findViewById(R.id.button_gps);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        if(!enabled){
-            Intent gpsintent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(gpsintent);
+        if (!enabled) {
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent gpsintent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(gpsintent);
+                }
+            });
+        } else {
+            Toast.makeText(this, "GPS Enabled", Toast.LENGTH_SHORT).show();
         }
-        else {
-            Toast.makeText(context, "GPS Enabled", Toast.LENGTH_SHORT).show();
+
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
         }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location!=null)
+        {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        }
+        else
+        {
+            latitude.setText("Location not available");
+            longitude.setText("Location not available");
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        latitude.setText(String.valueOf(lat));
+        longitude.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enable New Provider", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disable Provider", Toast.LENGTH_SHORT).show();
+    }
+
+    public LatLng getLocationFromAddress(Context context, String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng p1 = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null) {
+                return null;
+            }
+            Address location = address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            latitude.setText((int) location.getLatitude());
+            p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+        }
+        return p1;
     }
 }
